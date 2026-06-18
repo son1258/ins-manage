a09e164497fae3a833c8fa9c823b93bf431892e8
son1258 <dtswork88@gmail.com>
2026-06-19 01:19:43 +0700

    update format date and price colum

--- app/[locale]/(pages)/dashboard/orders/page.tsx
+++ app/[locale]/(pages)/dashboard/orders/page.tsx
@@ -477,10 +477,10 @@ export default function Declarations() {
                                                 {getPlanLabel(order)}
                                             </td>
                                             <td className="px-4 py-3 text-gray-600">{order.ld_name}</td>
-                                            <td className="px-4 py-3 text-gray-600">{dayjs(order.ld_dob).format("DD-MM-YYYY")}</td>
+                                            <td className="px-4 py-3 text-gray-600">{dayjs(order.ld_dob).format("DD/MM/YYYY")}</td>
                                             <td className="px-4 py-3 text-gray-600">{order.ld_maso_bhxh}</td>
                                             <td className="px-4 py-3 text-gray-600">{dayjs(order.created_at).format("DD/MM/YYYY")}</td>
-                                            <td className="px-4 py-3 text-gray-600">{order.billing_date}</td>
+                                            <td className="px-4 py-3 text-gray-600">{dayjs(order.billing_date).format("DD/MM/YYYY")}</td>
                                             <td className="px-4 py-3 text-center">
                                                 {(() => {
                                                     const { bg, label } = getPaymentStatus(order.status)
@@ -504,8 +504,12 @@ export default function Declarations() {
                                             {Number(formData.serviceCode) != SERVICE_CODE.BHXH ? (
                                                 <td className="px-4 py-3 border-r border-white text-left">{`${order.muc_dong_hgd}%`}</td>
                                             ) : (<></>)}
-                                            <td className="px-4 py-3 text-right text-teal-600 font-bold">{formatVND(order.amount)}</td>
-                                            <td className="px-4 py-3 text-right text-teal-600 font-bold">{formatVND(order.base_amount)}</td>
+                                            <td className="px-4 py-3 text-right text-teal-600 font-bold">
+                                                {Number(formData.serviceCode) != SERVICE_CODE.BHXH ? formatVND(order.amount) : formatVND(order.base_amount)}
+                                            </td>
+                                            <td className="px-4 py-3 text-right text-teal-600 font-bold">
+                                                {Number(formData.serviceCode) != SERVICE_CODE.BHXH ? formatVND(order.base_amount) : formatVND(order.amount)}
+                                            </td>
                                         </tr>
                                     ))}
                                 </tbody>
